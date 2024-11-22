8f1bb1212788741490dc4491835359762649ebad
mbrbharath <mbharathreddy1997@gmail.com>
2024-11-22 09:14:50 +0000

    add the file

---
+++ a3.sh
@@ -0,0 +1,9 @@
+why we need vsc code
+- to keep the code and track the code
+vsc is used in software industry where developer/teams keep source code changes at one place
+- vcs helps to keep track of collections of files
+- support creation of different version of collection of files
+- these are stored at a place called as repository
+- high resolution images and video it wont try by vcs
+- allows teams work on source code simutlanesouly
+
